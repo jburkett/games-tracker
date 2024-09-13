b4fd7373763cdb05492c12452f0a44ce6f6f4ef5
jburkett <1856722+jburkett@users.noreply.github.com>
2024-09-12 20:05:41 -0500

    add build and test workflow

--- website/GamesTracker.Web/Pages/Index.cshtml.cs
+++ website/GamesTracker.Web/Pages/Index.cshtml.cs
@@ -3,16 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GamesTracker.Web.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    public List<GameModel> Games { get; set; } = [];
 
     public void OnGet()
     {
+        Games =
+        [
+            new GameModel { Id = 1, Name = "Catan", Description = "A simple board game" },
+            new GameModel { Id = 2, Name = "Risk", Description = "An absurd board game" },
+            new GameModel { Id = 3, Name = "Scrabble", Description = "A game of words" }
+        ];
+    }
+
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
