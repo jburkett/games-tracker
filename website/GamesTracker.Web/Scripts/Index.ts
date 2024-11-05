bd4ef71cee489c2198593633a013f9ca469af714
jburkett <1856722+jburkett@users.noreply.github.com>
2024-11-05 10:45:21 -0600

    Communicating with server

---
+++ website/GamesTracker.Web/Scripts/Index.ts
@@ -0,0 +1,44 @@
+class Game {
+    id: number;
+    name: string;
+    description: string;
+
+    constructor(id: number, name: string, description: string) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
+
+const fetchGameDetails = async (id: number): Promise<Game> => {
+    const url = `/api/Games/${id}`; // Root-relative URL
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Game Details:", data);
+            return new Game(data.id, data.name, data.description);
+        } else {
+            console.error(`HTTP error! status: ${response.status}`);
+            return null;
+        }
+    } catch (error) {
+        console.error("Could not fetch game details for ID", id, ":", error);
+        return null;
+    }
+}
+
+const getThisGame = async function (this: HTMLAnchorElement): Promise<void> {
+    let gameId: number = parseInt(this.dataset.edit as string, 10);
+    let theGame: Game = await fetchGameDetails(gameId);
+
+    alert(theGame.name)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('[data-edit]') as NodeListOf<HTMLAnchorElement>;
+    for(const button of buttons) {
+        button.onclick = getThisGame;
+    }
+});
