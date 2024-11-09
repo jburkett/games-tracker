9015b953a025d2b6725be5ea04d3064cdf59b464
jburkett <1856722+jburkett@users.noreply.github.com>
2024-11-08 18:37:45 -0600

    Extracted GetGamesTests to separate file

---
+++ website/GamesTracker.Web.UnitTests/Api/GamesControllerTests/GetGamesTests.cs
@@ -0,0 +1,42 @@
+using GamesTracker.Core;
+using GamesTracker.Web.Api;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+
+namespace GamesTracker.Web.UnitTests.Api.GamesControllerTests;
+
+public class GetGamesTests
+{
+    [Fact]
+    public void Returns_200_On_Success()
+    {
+        var controller = new GamesController(GetSimpleGameManager());
+
+        var response = controller.GetGames() as OkObjectResult;
+
+        response.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Returns_All_Games()
+    {
+        var controller = new GamesController(GetSimpleGameManager());
+
+        var response = controller.GetGames() as OkObjectResult;
+        var actual = response!.Value as Game[];
+
+        actual!.Length.Should().Be(3);
+    }
+
+    private static IGameManager GetSimpleGameManager()
+    {
+        var gameManager = Substitute.For<IGameManager>();
+        gameManager.GetGames().Returns([
+            new Game { Id = 1, Name = "Chess" },
+            new Game { Id = 2, Name = "Risk" },
+            new Game { Id = 3, Name = "Scrabble" }
+        ]);
+
+        return gameManager;
+    }
+}
